f4c9750c6e36ffc107a9cdb235ebbc354c9ca509
Joe Corall <jjc223@lehigh.edu>
2025-11-04 16:49:20 -0500

    debug failure

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 set -eou pipefail
 
-max_attempts=25
+max_attempts=10
 attempt=0
 
 while [ $attempt -lt $max_attempts ]; do
@@ -11,12 +11,15 @@ while [ $attempt -lt $max_attempts ]; do
   
   sleep 10
   
-  if curl -sf http://localhost | grep "<img" | grep -q "Open Journal Systems"; then
+  if curl -sf http://localhost:8888/ | grep "<img" | grep -q "Open Journal Systems"; then
     echo "OJS is up!"
     exit 0
   fi
+  sleep 60
+  docker compose logs ojs --tail 20
 done
+
+docker compose logs ojs
 
 echo "Failed to detect OJS after $max_attempts attempts"
 exit 1
-
