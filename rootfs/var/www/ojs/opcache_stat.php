1e25f56589894601df93df7ef64d49ffdc2db6d7
Joe Corall <jjc223@lehigh.edu>
2025-11-20 11:25:31 -0500

    Do not log healthchecks

--- rootfs/var/www/ojs/opcache_stat.php
+++ rootfs/var/www/ojs/opcache_stat.php
@@ -1,10 +1,5 @@
 <?php
 
-if (!str_starts_with($_SERVER['REMOTE_ADDR'], '128.180.')) {
-    http_response_code(403);
-    die('Access denied');
-}
-
 header('Content-Type: application/json');
 $status=opcache_get_status(FALSE);
 echo json_encode($status, JSON_PRETTY_PRINT);
